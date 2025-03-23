e9e67c0d00c46e39b4e78c02b45f37f7d0e131b5
Chris Bielow <chris.bielow@bsc.fu-berlin.de>
2025-03-23 09:00:02 +0000

    version 0.5.5

--- inst/doc/validation_guide.R
+++ inst/doc/validation_guide.R
@@ -1,22 +1,22 @@
 ## ----eval=FALSE---------------------------------------------------------------
-#  library(rmzqc)
-#  mzQC_filename = system.file("testdata/test.mzQC", package="rmzqc")
-#  #mzQC_filename = "C:\\dev\\rmzqc\\inst\\testdata\\test.mzQC"
-#  res = validateFromFile(mzQC_filename)
-#  res
-#  
+# library(rmzqc)
+# mzQC_filename = system.file("testdata/test.mzQC", package="rmzqc")
+# #mzQC_filename = "C:\\dev\\rmzqc\\inst\\testdata\\test.mzQC"
+# res = validateFromFile(mzQC_filename)
+# res
+# 
 
 ## ----eval=FALSE---------------------------------------------------------------
-#  library(rmzqc)
-#  mzQC_strings = readLines(system.file("testdata/test.mzQC", package="rmzqc"))
-#  res = validateFromString(mzQC_strings)
-#  res
-#  
+# library(rmzqc)
+# mzQC_strings = readLines(system.file("testdata/test.mzQC", package="rmzqc"))
+# res = validateFromString(mzQC_strings)
+# res
+# 
 
 ## ----eval=FALSE---------------------------------------------------------------
-#  library(rmzqc)
-#  mzQC_root = rmzqc::readMZQC(system.file("testdata/test.mzQC", package="rmzqc"))
-#  res = validateFromObj(mzQC_root)
-#  res
-#  
+# library(rmzqc)
+# mzQC_root = rmzqc::readMZQC(system.file("testdata/test.mzQC", package="rmzqc"))
+# res = validateFromObj(mzQC_root)
+# res
+# 
 
